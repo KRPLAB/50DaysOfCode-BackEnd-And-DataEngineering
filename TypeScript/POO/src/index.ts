fceb61c6546c82e159431af9ae39c2e27d6c41f9
krplab <kauan.r.paulino@gmail.com>
2024-09-17 21:09:56 -0300

    Introdução a POO - Método COnstrutor

--- TypeScript/POO/src/index.ts
+++ TypeScript/POO/src/index.ts
@@ -2,14 +2,31 @@
 // class - encapsulamento/agrupamento de métodos 
 
 class Person {
-    name: string = "Kauan";
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
 
     presentation(message: string) {
         console.log(message);
     }
+
+    sayMyName() {
+        console.log(`Me chamo ${this.name}`);
+    }
 }
 
-const person = new Person();
+const person1 = new Person("Kauan");
 
-console.log(person.name);
-person.presentation("Olá pessoal, me chamo Kauan!");
+console.log(person1.name);
+person1.presentation("Olá pessoal!");
+person1.sayMyName();
+
+const person2 = new Person("Osvaldo");
+
+console.log(person2.name);
+person2.presentation("Olá pessoal!");
+person2.sayMyName();
+
+
