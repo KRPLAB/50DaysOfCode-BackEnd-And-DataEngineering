96afde9243234cbb3fa482202f6b0d167c41cab2
krplab <kauan.r.paulino@gmail.com>
2024-09-23 20:41:46 -0300

    Instanciando classe e testando métodos e variáveis

--- TypeScript/Atividade-Criando-Classe-Student/src/index.ts
+++ TypeScript/Atividade-Criando-Classe-Student/src/index.ts
@@ -1,3 +1,9 @@
 import { Student } from "./student";
 
-const student1 = new Student()
+const student1 = new Student("Kauan", "M4");
+
+student1.sayHello();
+
+student1.setGrade(10);
+
+student1.getGrade();
